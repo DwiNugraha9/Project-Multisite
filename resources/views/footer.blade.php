43402323a4092836da8eff94cd51c86b43aef177
Ghalib Abdillah <zuhairsyahf@gmail.com>
2022-05-29 22:03:02 +0700

    penambahan footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/footer.css">
+    <script src="https://kit.fontawesome.com/ff1fe4e774.js" crossorigin="anonymous"></script>
+
+    <title>footer</title>
+</head>
+<body>
+    
+
+<!-- Site footer -->
+  <footer class="site-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-6">
+          <h6>Digital Multisite </h6>
+          <p class="text-justify">Digital Multisite adalah layanan dengan menggunakan berbagai channel yang dijadikan strategi perusahaan untuk meningkatkan kepuasan pelanggan, penjualan perusahaan, dan kebutuhan perusahaan lainnya. Digital Multisite adalah metode integratif antara hubungan pelanggan dengan perusahaan.</p>
+        </div>
+
+        <div class="col-xs-6 col-md-3">
+          <h6>Fitur</h6>
+          <ul class="footer-links">
+            <li><a href="ke_atas"style="text-decoration: none;">Katalog</a></li>
+            <li><a href="ke_atas"style="text-decoration: none;">Pesanan</a></li>
+            <li><a href="ke_atas"style="text-decoration: none;">Persediaan</a></li>
+            <li><a href="ke_atas"style="text-decoration: none;">Manajemen Gudang</a></li>
+            <li><a href="ke_atas"style="text-decoration: none;">Akuntansi</a></li>
+          </ul>
+        </div>
+
+        <div class="col-xs-6 col-md-3">
+          <h6>Contact Us</h6>
+          <ul class="footer-links">
+            <li><i class="fa-solid fa-phone">&nbsp;&nbsp;</i>021 - 6628728</li>
+            <li><i class="fa-solid fa-envelope">&nbsp;&nbsp;</i>zuhairsyahf@gmail.com</li>
+            <li><i class="fa-solid fa-location-dot">&nbsp;&nbsp;</i>Jalan Kupie Atjeh No. 20, Medan</li>
+          </ul>
+        </div>
+      </div>
+         <h6 style="color: #fff;">FIND US ON MAPS</h6>
+		    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d254832.60117458165!2d98.52940633788393!3d3.6422756426297282!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x303131cc1c3eb2fd%3A0x23d431c8a6908262!2sMedan%2C%20Kota%20Medan%2C%20Sumatera%20Utara!5e0!3m2!1sid!2sid!4v1653454875759!5m2!1sid!2sid" style="width: 100%; height: 195px;" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+      <hr>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-sm-6 col-xs-12">
+          <p class="copyright-text">Copyright &copy; 2022 All Rights Reserved by PT Digital Multisite Indonesia.
+          </p>
+        </div>
+
+        <div class="col-md-4 col-sm-6 col-xs-12">
+          <ul class="social-icons">
+            <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+            <li><a class="instagram" href="#"><i class="fa fa-instagram"></i></a></li>
+            <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+          </ul>
+        </div>
+      </div>
+    </div>
+</footer>
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
